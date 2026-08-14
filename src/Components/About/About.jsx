04244ca34153f4c9e9437bf564671b51ed390bfe
Reynflrs5 <fjashleyrain@gmail.com>
2026-08-14 21:29:31 +0800

    new profile

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,11 @@
 import { FiArrowUpRight } from "react-icons/fi";
 import { motion } from "framer-motion";
-import reynimg from "../../assets/reyn1.jpe";
+import reynimg from "../../assets/profile.png";
 import "./About.css";
 
 export default function About() {
   const links = {
-    facebook: "https://www.facebook.com/reynflrs",
+    facebook: "https://www.facebook.com/Reyn12345",
     instagram: "https://instagram.com/reynflrs",
     email: "mailto:fjashleyrain@gmail.com",
   };
@@ -37,19 +37,19 @@ export default function About() {
 
       <div className="about-stats-container">
         <div className="about-stat-item">
-          <h4>4th<FiArrowUpRight size={14} color="#a3a3a3"/></h4>
+          <h4>4th<FiArrowUpRight size={14} color="#a3a3a3" /></h4>
           <p>YEAR IT STUDENT</p>
         </div>
         <div className="about-stat-item">
-          <h4>4+<FiArrowUpRight size={14} color="#a3a3a3"/></h4>
+          <h4>4+<FiArrowUpRight size={14} color="#a3a3a3" /></h4>
           <p>PROJECTS</p>
         </div>
         <div className="about-stat-item">
-          <h4>13<FiArrowUpRight size={14} color="#a3a3a3"/></h4>
+          <h4>13<FiArrowUpRight size={14} color="#a3a3a3" /></h4>
           <p>TECHS LEARNED</p>
         </div>
         <div className="about-stat-item">
-          <h4>100%<FiArrowUpRight size={14} color="#a3a3a3"/></h4>
+          <h4>100%<FiArrowUpRight size={14} color="#a3a3a3" /></h4>
           <p>DEDICATION</p>
         </div>
       </div>
